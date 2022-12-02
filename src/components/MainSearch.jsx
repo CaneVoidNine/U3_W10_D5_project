93fb4efba42960c7e2e5155d67c3f31396adce32
Damian Olszewski <damianolszewski25@gmail.com>
2022-12-02 16:51:24 +0100

    mess

--- src/components/MainSearch.jsx
+++ src/components/MainSearch.jsx
@@ -1,13 +1,14 @@
 import { Col, Row, Container, Form } from "react-bootstrap";
 import { useEffect, useState } from "react";
-import City from "./City";
 
 export default function MainSearch() {
   const [query, setQuery] = useState("");
-  const [cities, setCities] = useState([]);
+  const [cities, setCities] = useState({});
 
   const baseEndpoint = "https://api.openweathermap.org/data/2.5/weather?q=";
-
+  useEffect = () => {
+    handleSubmit();
+  };
   const handleChange = (e) => {
     setQuery(e.target.value);
   };
@@ -19,9 +20,9 @@ export default function MainSearch() {
         baseEndpoint + query + ",&APPID=4f6026bc1315d42e2c0879ac5fb74945"
       );
       if (response.ok) {
-        const data = await response.json();
-        console.log(data);
-        setCities(data);
+        const cities = await response.json();
+        console.log(cities);
+        setCities(cities);
       } else {
         alert("Error fetching results");
       }
@@ -31,7 +32,7 @@ export default function MainSearch() {
   };
 
   return (
-    <div style={{ height: "200vh" }} className="bg-light">
+    <div className="bg-light">
       <Container>
         <Row>
           <Col md={12} className="d-flex justify-content-center py-1 mt-5">
@@ -56,35 +57,18 @@ export default function MainSearch() {
               <h4>Temperature: </h4>
               <h4>Humidity: </h4>
               <h4>Pressure: </h4>
-              <h4>Sunrise: </h4>
-              <h4>Sunset: </h4>
-              <h4>Timezone: </h4>
               <h4>Visibility: </h4>
-              <h4>Wind: </h4>
+              <h4>Wind Speed: </h4>
             </Col>
             <Col className="mx-auto my-5" md={2}>
-              <h4>{cities.name}</h4>
-              <h4></h4>
-              <h4></h4>
-              <h4></h4>
-            </Col>
-            <Col className="mx-auto my-5" md={2}>
-              <h4>{cities}</h4>
-              <h4></h4>
-              <h4></h4>
-              <h4></h4>
-            </Col>
-            <Col className="mx-auto my-5" md={2}>
-              <h4>{cities}</h4>
-              <h4></h4>
-              <h4></h4>
-              <h4></h4>
-            </Col>
-            <Col className="mx-auto my-5" md={2}>
-              <h4>{cities}</h4>
-              <h4></h4>
-              <h4></h4>
-              <h4></h4>
+              <h4>
+                {cities.name}, {cities.sys.country}
+              </h4>
+              <h4>{cities.main.temp} F</h4>
+              <h4>{cities.main.humidity} %</h4>
+              <h4>{cities.main.pressure} Pa</h4>
+              <h4>{cities.visibility}</h4>
+              <h4>{cities.wind.speed}</h4>
             </Col>
           </Row>
         </Container>
